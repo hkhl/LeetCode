9fecb09a6398c4aeaa86c5d2ffd9347eb4ca3537
hkhl <dhkhl235@gmail.com>
2018-05-29 11:48:02 +0800

    BInary Tree Tilt

--- 563. Binary Tree Tilt.cpp
+++ 563. Binary Tree Tilt.cpp
@@ -29,6 +29,19 @@
  * };
  */
 
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class Solution {
 public:
 
@@ -62,4 +75,30 @@ public:
 
         return abs(leftsum - rightsum) + findTilt(root->left) + findTilt(root->right);
     }
+
+    /*或者计算的过程中把所有的倾斜度都计算出来
+
+    public class Solution {
+    int result = 0;
+
+    public int findTilt(TreeNode root) {
+        postOrder(root);
+        return result;
+    }
+
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+
+        result += Math.abs(left - right);
+
+        return left + right + root.val;
+    }
+}
+    */
 };
+
+
+
