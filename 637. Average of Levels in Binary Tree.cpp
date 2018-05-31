32fe264fd93ed7573d03b53c4039a36e7f9d1b1e
hkhl <dhkhl235@gmail.com>
2018-05-31 15:52:41 +0800

    Average of Levels in Binary

--- 637. Average of Levels in Binary Tree.cpp
+++ 637. Average of Levels in Binary Tree.cpp
@@ -65,3 +65,26 @@ public:
         return vd;
     }
 };
+
+
+
+//其他解法, 层序遍历的时候计算出结果(for循环)
+
+public List<Double> averageOfLevels(TreeNode root) {
+    List<Double> result = new ArrayList<>();
+    Queue<TreeNode> q = new LinkedList<>();
+
+    if(root == null) return result;
+    q.add(root);
+    while(!q.isEmpty()) {
+        int n = q.size();
+        double sum = 0.0;
+        for(int i = 0; i < n; i++) {
+            TreeNode node = q.poll();
+            sum += node.val;
+            if(node.left != null) q.offer(node.left);
+            if(node.right != null) q.offer(node.right);
+        }
+        result.add(sum / n);
+    }
+    return result;
